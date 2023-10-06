40cef11bbdb65a21a10762ec73c01bfbd52050d3
Oksana Melnyk <oks.melnuk@gmail.com>
2023-10-06 23:35:37 +0200

    changed

--- src/js/main.js
+++ src/js/main.js
@@ -1,47 +1,71 @@
-// Мобильное меню бургер
-function burgerMenu() {
-    const burger = document.querySelector('.burger')
-    const menu = document.querySelector('.menu')
-    const body = document.querySelector('body')
-    burger.addEventListener('click', () => {
-        if (!menu.classList.contains('active')) {
-            menu.classList.add('active')
-            burger.classList.add('active')
-            body.classList.add('locked')
-        } else {
-            menu.classList.remove('active')
-            burger.classList.remove('active')
-            body.classList.remove('locked')
-        }
-    })
-    // Вот тут мы ставим брейкпоинт навбара
-    window.addEventListener('resize', () => {
-        if (window.innerWidth > 991.98) {
-            menu.classList.remove('active')
-            burger.classList.remove('active')
-            body.classList.remove('locked')
-        }
-    })
-}
-burgerMenu()
-
-//Price Page
-const checkbox = document.querySelector('#checkbox')
-const label = document.querySelector('#label')
-checkbox.addEventListener('change', showPlan)
-
-function showPlan() {
-    label.classList.toggle ('selected');
-    displayPrice();
+function initializeEvents() {
+    burgerMenu();
+    changeMenuColor();
+    
+    // Checking if we are on the "price.html" page
+    if (window.location.href.includes('price.html')) {
+        price();
+    }
+    faq();
 }
 
-function displayPrice() {
-    const prices = document.querySelectorAll('.subscription__price')
-    prices.forEach(price => {
-        price.classList.toggle('show-price')
+initializeEvents();
+
+//Backlight active menu item
+function changeMenuColor() {
+    const currentPage = window.location.href;
+    const menuLinks = document.querySelectorAll('.menu__item-link');
+
+    menuLinks.forEach(link => {
+        link.style.borderBottom = 'none'; // Скидання border-bottom для всіх елементів
+        const linkHref = link.getAttribute('href');
+        if (currentPage.includes(linkHref)) {
+            link.style.borderBottom = '2px solid #E14817';
+        }
     });
 }
 
+//Mobile menu
+function burgerMenu() {
+    const burger = document.querySelector('.burger');
+    const menu = document.querySelector('.menu');
+    const body = document.querySelector('body');
+
+    const toggleMenu = () => {
+        menu.classList.toggle('active');
+        burger.classList.toggle('active');
+        body.classList.toggle('locked');
+    };
+
+    const resizeHandler = () => {
+        if (window.innerWidth > 991.98) {
+            menu.classList.remove('active');
+            burger.classList.remove('active');
+            body.classList.remove('locked');
+        }
+    };
+
+    burger.addEventListener('click', toggleMenu);
+    window.addEventListener('resize', resizeHandler);
+}
+
+// Price Page
+function price() {
+    const checkbox = document.querySelector('#checkbox');
+    const label = document.querySelector('#label');
+
+    checkbox.addEventListener('change', showPlan);
+
+    function showPlan() {
+        label.classList.toggle('selected');
+        const prices = document.querySelectorAll('.subscription__price');
+        prices.forEach(price => {
+            price.classList.toggle('show-price');
+        });
+    }
+}
+
+//Accordion
 function faq() {
     const items = document.querySelectorAll('.faq__item-trigger');
     items.forEach(item => {
@@ -58,5 +82,3 @@ function faq() {
         });
     });
 }
-faq();
- 
